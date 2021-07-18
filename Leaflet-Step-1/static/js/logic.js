b538408e821e70fd1502f0979684cbff9ebeb38b
DESKTOP-LL56H96\marc_ <marc_e_leslie@yahoo.com>
2021-07-18 16:58:31 -0400

    Still messing around - will now try out Jamies video

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,39 +1,13 @@
 let dataURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson"; 
 
+
+
 d3.json(dataURL).then(function(data) // THIS IS WHAT MAKES THE MAP WORK
 { 
     createFeatures(data.features);
-    console.log(data.features); 
+    // console.log('data features', data.features);
 }); 
 
-
-//DO YOUR CIRCLES FOR LOOP HERE LIKE FUNCTION MAG COLORS = 
-//MAKE CIRCLE RADIUS DEPENDENT ON MAGNITUTE 
-//HOW THE FUCK?????
-
-function createFeatures(yourData)
-{
-    function onEachFeature(feature, layer)
-        {  //Pop up must have MAGNITURE, LOCATION, DEPTH 
-            layer.bindPopup("<h3>" + "Location:" +feature.properties.place + 
-            "</h3><hr><p>" + "Magnitude:" + feature.properties.mag + 
-            "</p><hr><p>" + "Depth:" + feature.geometry.coordinates[2] + "</p>"); //The [2] takes you to depth 
-        }
-    let quakes = L.geoJson(yourData, 
-        {
-            onEachFeature: onEachFeature
-        });
-    
-    createMap(quakes); 
-}
-
-
-   // I NEED SOME WAY TO LOOK AT THE RANGE OF MAG AND DEPTH AND THIS DOES NOT WORKT
-//    let depthMax = d3.json.max(yourData, function(d) {return +d.geometry}); 
-//    console.log(depthMax); 
-
-// Circle SIZE = quake MAG (> mag = larger circle)
-// Circle COLOR = quake DEPTH (> depth = darker circle)
 
 
 function createMap(quakes) {
@@ -51,12 +25,86 @@ function createMap(quakes) {
   
    
     // Create our map, giving it the streetmap and earthquakes layers to display on load
-    var myMap = L.map("map", 
-    {
-      center: [37.09, -95.71],
-      zoom: 5,
-      layers: [streetmap, quakes]
-    });
-
+    L.map("map",
+        {
+            center: [37.09, -95.71],
+            zoom: 5,
+            layers: [streetmap, quakes]
+        });
 }
+
+
+
+
+// d3.json(dataURL).then(function(data) {
+//   function styleInfo(feature) {
+//     return {
+//     };
+//   }
+//   // This function determines the color of the marker based on the magnitude of the earthquake.
+//   // RANGE FOR DEPTH IS 90, 70-90, 50-70, 30-50, 10-30, and a default 
+//   function getColor(depth) {
+//     switch (true) {
+    
+//     }
+// )}; 
+
+
+
+//DO YOUR CIRCLES FOR LOOP HERE LIKE FUNCTION MAG COLORS = 
+//MAKE CIRCLE RADIUS DEPENDENT ON MAGNITUTE 
+
+
+function createFeatures(yourData) {
+    // console.log('your data', yourData);
+    // let quakes = L.geoJson(
+    //         yourData,
+    //         {  onEachFeature: onEachFeature }
+    //     );
+
+    let quakeArr = [];
+    for (let i = 0; i < yourData.length; i++) {
+        let quake = L.circle(yourData[i].geometry.coordinates, {
+            _latlng: yourData[i].geometry.coordinates,
+            stroke: false,
+            fillOpacity: 0.75,
+            color: "orange",
+            fillColor: "orange",
+            radius: yourData[i].properties.mag * 10000
+        });
+        // L.latLng(yourData[i].geometry.coordinates);
+        console.log('quake', quake);
+        // console.log('coordinates', yourData[i].geometry.coordinates);
+        quakeArr.push(quake);
+    }
+
+    let quakes = L.geoJson(yourData, { onEachFeature: onEachFeature });
+    createMap(quakes);
+}
+
+// CREATE POP UP LABELS 
+function onEachFeature(feature, layer) {  //Pop up must have MAGNITURE, LOCATION, DEPTH 
+    layer.bindPopup("<h3>" + "Location:" + feature.properties.place +
+        "</h3><hr><p>" + "Magnitude:" + feature.properties.mag +
+        "</p><hr><p>" + "Depth:" + feature.geometry.coordinates[2] + "</p>"); //The [2] takes you to depth 
+}
+
+function createCircles(mag, depth) {
+    console.log('magnitude', mag);
+    console.log('depth', depth);
+}
+
+
+  
+
+
+   // I NEED SOME WAY TO LOOK AT THE RANGE OF MAG AND DEPTH AND THIS DOES NOT WORK
+//    let depthMax = d3.json.max(yourData, function(d) {return +d.geometry}); 
+//    console.log(depthMax); 
+
+// Circle SIZE = quake MAG (> mag = larger circle)
+// Circle COLOR = quake DEPTH (> depth = darker circle)
+
+
+
   
